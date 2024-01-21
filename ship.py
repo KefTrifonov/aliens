79dd100694330d60696a272eef885f402777c81b
KefTrifonov <andrewtrifonov.s@yandex.ru>
2024-01-21 20:49:53 +0300

    Ship moving refactor Added fulscreen mode

--- ship.py
+++ ship.py
@@ -25,9 +25,9 @@ class Ship():
 
     def update(self):
         """Updates ship moving counting the flag (moving = False)"""
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > self.screen_rect.left:
             self.x -= self.settings.ship_speed
         self.rect.x = self.x
 
